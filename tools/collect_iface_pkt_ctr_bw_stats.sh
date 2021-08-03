b82fd44db9e3a57c7968fc81ccf231951cfe946e
PvS <pvsconext@gmail.com>
2021-08-03 20:19:44 -0300

    bug fix

--- tools/collect_iface_pkt_ctr_bw_stats.sh
+++ tools/collect_iface_pkt_ctr_bw_stats.sh
@@ -1,41 +1,56 @@
 if [ $# -ne 2 ]; then
         echo
-        echo "usage: $0 [network-interface] [interval(seconds)]"
+        echo "usage: $0 [network-interface] [interval(miliseconds)]"
         echo
-        echo e.g. $0 eth0 0.1
+        echo e.g. $0 eth0 100
         echo
-        echo shows packets-per-second
+        echo shows packets-per-second. interval MUST BE multiple of 100
         exit
 fi
 
 IF=$1
 INTERVAL=$2
+SLEEP_INT=`echo $2/1000 | bc -l`
 
-echo "Experiment began `date`";
+echo "Experiment began `date` (interval $INTERVAL sleep_int $SLEEP_INT)";
+
+# fetch first data
+DATA=`cat /sys/class/net/$IF/statistics/rx_packets \
+	/sys/class/net/$IF/statistics/tx_packets \
+	/sys/class/net/$IF/statistics/rx_bytes \
+	/sys/class/net/$IF/statistics/tx_bytes`;
+set -- $DATA;
+Rcurr=$1
+Tcurr=$2
+Rcurrbytes=$3
+Tcurrbytes=$4
+
 while true
 do
-        # fetch data
-	DATA=`cat /sys/class/net/$IF/statistics/rx_packets /sys/class/net/$IF/statistics/tx_packets /sys/class/net/$IF/statistics/rx_bytes /sys/class/net/$IF/statistics/tx_bytes`;
+	sleep $SLEEP_INT
+
+	#save previous data
+        Rprev=$Rcurr
+	Tprev=$Tcurr
+	Rprevbytes=$Rcurrbytes
+	Tprevbytes=$Tcurrbytes
+
+        # fetch next data
+	DATA=`cat /sys/class/net/$IF/statistics/rx_packets \
+		/sys/class/net/$IF/statistics/tx_packets \
+		/sys/class/net/$IF/statistics/rx_bytes \
+		/sys/class/net/$IF/statistics/tx_bytes`;
         set -- $DATA;
-        R1=$1
-        T1=$2
-        R1bytes=$3
-	T1bytes=$4
-        sleep $INTERVAL
-	
-	#fetch delta...
-	DATA=`cat /sys/class/net/$IF/statistics/rx_packets /sys/class/net/$IF/statistics/tx_packets /sys/class/net/$IF/statistics/rx_bytes /sys/class/net/$IF/statistics/tx_bytes`;
-	set -- $DATA;
-        R2=$1
-        T2=$2
-        R2bytes=$3
-        T2bytes=$4
+	Rcurr=$1
+	Tcurr=$2
+	Rcurrbytes=$3
+	Tcurrbytes=$4
 
 	#compute the diff
-	TXPPS=$(($T2 - $T1))
-        RXPPS=$(($R2 - $R1))
-        TXBPS=$(($T2bytes - $T1bytes))
-        RXBPS=$(($R2bytes - $R1bytes))
+	TXPPS=$((($Tcurr - $Tprev)*1000/$INTERVAL))
+	RXPPS=$((($Rcurr - $Rprev)*1000/$INTERVAL))
+	TXBPS=$((($Tcurrbytes - $Tprevbytes)*1000/$INTERVAL))
+	RXBPS=$((($Rcurrbytes - $Rprevbytes)*1000/$INTERVAL))
 
 	#output to stdout
         echo "`date +%s` TX $IF: $TXPPS pkts/s $TXBPS bps RX $IF: $RXPPS pkts/s $RXBPS bps"
